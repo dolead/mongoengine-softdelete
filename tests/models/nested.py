dbcf0e527dcfeb8e4f3f2b91f41d03d0320aff68
hirsch-b <benjamin.hirschfield@dolead.com>
2022-01-26 01:50:48 +0100

    wip

--- tests/models/nested.py
+++ tests/models/nested.py
@@ -1,13 +1,30 @@
+from enum import Enum
 from mongoengine import fields
 
-from mongoengine_softdelete.document import SoftDeleteNoCacheDocument
+from mongoengine_softdelete.document import SoftDeleteDocument, SoftDeleteNoCacheDocument
 
 
-class Nested(SoftDeleteNoCacheDocument):
+class Nestor(SoftDeleteNoCacheDocument):
     meta = {
         'collection': 'nested',
-        'soft_delete': {'dikt__hide_me__contains': 'yes'},
+        'soft_delete': {'dikt': {'hide_me': 'yes'}},
+        # 'soft_delete': {'dikt__hide_me__contains': 'yes'},
         'strict': False
     }
 
     dikt = fields.DictField()
+
+
+class Status(Enum):
+    OPEN = "OPEN"
+    CLOSED = "CLOSED"
+
+
+class Naster(SoftDeleteDocument):
+    meta = {
+        'collection': 'naster',
+        'soft_delete': {'status': Status.CLOSED},
+        'strict': False
+    }
+
+    status = fields.EnumField(Status)
--- tests/test_subkey_queries.py
+++ tests/test_subkey_queries.py
@@ -1,21 +1,31 @@
 from tests.base import TestCase
-from tests.models.nested import Nested
+from tests.models.nested import Nestor
 
 class TestSubKeyQueries(TestCase):
     def setUp(self):
-        Nested.drop_collection()
+        Nestor.drop_collection()
 
     def tearDown(self):
-        Nested.drop_collection()
+        Nestor.drop_collection()
 
     def test_created_nested_dict(self):
-        nstor = Nested(dikt={"hello": "world"}).save()
-        assert 1 == Nested.objects().count()
+        nstor = Nestor(dikt={"hello": "world"}).save()
+        assert 1 == Nestor.objects().count()
 
         nstor.dikt["hide_me"] = "maybe"
         nstor.save()
-        assert 1 == Nested.objects().count()
+        assert 1 == Nestor.objects().count()
 
-        nstor.dikt["hide_me"] = "yes"
-        nstor.save()
-        assert 0 == Nested.objects().count()
+        nstor.soft_delete()
+        assert 0 == Nestor.objects().count()
+        assert 1 == Nestor.objects().soft_deleted.count()
+
+        sd_nstor = Nestor.objects().soft_deleted.first()
+        sd_nstor.dikt["hide_me"] = "nope.avi"
+        sd_nstor.save()
+        sd_nstor.reload()
+        assert 1 == Nestor.objects().count()
+
+        # nstor.dikt["hide_me"] = "yes"
+        # nstor.save()
+        # assert 0 == Nestor.objects().count()
